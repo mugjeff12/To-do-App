cb9027d8cc165544e596fd180e2c56c587012d89
Mugdho <mugdhojeferson@gmail.com>
2024-11-25 17:05:28 +0600

    Added Edit and Todos Case

--- GUI.py
+++ GUI.py
@@ -5,8 +5,13 @@ import FreeSimpleGUI
 label = FreeSimpleGUI.Text("Type in a to-do")
 input_box = FreeSimpleGUI.InputText(tooltip="Enter To-do: ", key ='todo')
 add_button = FreeSimpleGUI.Button("Add")
+
+list_box = FreeSimpleGUI.Listbox(values=functions.get_todos(), key='todos'
+                                 ,enable_events = True,size=[45,10])
+edit_button = FreeSimpleGUI.Button('Edit')
+
 window=FreeSimpleGUI.Window('My To-do App',
-                            layout=[[label],[input_box,add_button]],
+                            layout=[[label],[input_box,add_button],[list_box,edit_button]],
                             font=('Helvetica',20))
 #an instance of the Window type stored in the variable, window
 #one single [] means one single row
@@ -23,6 +28,23 @@ while True:
             new_todo = values['todo'] +'\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit =  values["todos"][0]
+            todo_to_add = values["todo"]
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index]=todo_to_add + '\n'
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        #case 'todos':
+        #window['todo'].update(value=values['todos'][0])
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
+
+
+
         case FreeSimpleGUI.WINDOW_CLOSED:
             break
 
